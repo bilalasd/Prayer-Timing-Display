2b547f4771e315bb36e6a2edda8a1627e7470ec2
bilalasd <bilal2206@gmail.com>
2018-04-20 16:18:30 -0500

    - Added 'call' to script

--- start-automatically.bat
+++ start-automatically.bat
@@ -1,5 +1,5 @@
 git pull
-npm install -g serve
+call npm install -g serve
 cd .\client\
 start serve .\build\ -p=3000
 "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:3000 --kiosk
