859ea23e3bf1a608a4bc6d71f3841b4fb2a718d9
Muhammad Balagamwala <bilal2206@gmail.com>
2018-04-22 16:38:48 -0500

    - added the line in the script to install packages in server

--- start-automatically.bat
+++ start-automatically.bat
@@ -1,6 +1,9 @@
 git pull
 call npm install -g serve
 start /min serve .\client\build\ -p=3000
+cd ./server/
+call npm install
+cd ..
 start /min node .\server\index.js
 timeout /t 15
 "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:3000 --kiosk
