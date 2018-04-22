d285f760531a649e5cdf209c64229d3af272439e
Muhammad Balagamwala <bilal2206@gmail.com>
2018-04-22 11:49:20 -0500

    - Changed the script to open the serve window minimized

--- start-automatically.bat
+++ start-automatically.bat
@@ -1,5 +1,5 @@
 git pull
 call npm install -g serve
-start serve .\client\build\ -p=3000
+start /min serve .\client\build\ -p=3000
 timeout /t 15
 "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:3000 --kiosk
